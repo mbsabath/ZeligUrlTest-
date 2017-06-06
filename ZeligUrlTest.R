8e0da156fb6ab7d49ba5345644470b884e3c2a64
mbsabath <bsabath1@gmail.com>
2017-06-06 11:56:35 -0400

    Initial Code Commit

---
+++ ZeligUrlTest.R
@@ -0,0 +1,37 @@
+library(xml2, rvest)
+
+test_web_vignettes <- function() {
+  ## Assumes that the current working directory is Zelig package top directory
+
+  
+  setwd("docs")
+  setwd("articles")
+  articles <- list.files(pattern = "html")
+  for (article in articles) {
+    links <- get_links(article)
+    for (link in links) {
+      ## do test on link
+      ## if link broken, add article and link to the dataframe storing them
+      
+    }
+  }
+  
+  ## Clean Up Code
+  setwd("..")
+  setwd("..")
+  
+  ## Return dataframe of broken links
+  
+}
+
+#' Parses an html document and returns a vector of all links in the document
+#' 
+#' 
+#' @author Ben Sabath
+#' @return Vector of urls
+#'
+get_links <- function(html_doc) {
+  
+  
+}
+
